9b9829abd770f6910d270d84e67f534e455ec918
anandsedulab <yadavanandw2@gmail.com>
2023-02-08 12:38:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder,FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  form: FormGroup;
+  submitted = false;
+  constructor(private fb:FormBuilder){
+
+  }
+
+  get fullname(){
+    return this.form.get('fullname');
+  }
+
+  ngOnInit()
+  {
+     this.form = this.fb.group(
+      {
+        fullname: ['', Validators.required],
+      }
+    );
+  }
+
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+ 
+  onSubmit(){
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+  }
 }
